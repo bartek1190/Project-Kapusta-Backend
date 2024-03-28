5439acbc356c8b23a764cc92f79943ede1356e77
Toy Santana António <toysan3@hotmail.com>
2024-03-28 19:07:25 +0100

    Update

--- src/middlewares/googleAuthMiddleware.js
+++ src/middlewares/googleAuthMiddleware.js
@@ -17,25 +17,26 @@ passport.use(
     async (request, accessToken, refreshToken, profile, done) => {
       const { id, emails } = profile;
       try {
-        let user = await User.findOne({ googleId: id });
+        let user = await User.findOne({ email: emails[0].value });
+        const avatarUrl = `https://ui-avatars.com/api/?name=${emails[0].value[0].toUpperCase()}&background=random&size=128`;
+
         if (!user) {
-          user = await User.findOne({ email: emails[0].value });
-          const firstLetter = emails[0].value[0].toUpperCase();
-          const avatarUrl = `https://ui-avatars.com/api/?name=${firstLetter}&background=random&size=128`;
-          if (!user) {
-            user = new User({
-              googleId: id,
-              email: emails[0].value,
-              avatarUrl: avatarUrl,
-            });
-          } else {
-            user.googleId = id;
-          }
+          user = new User({
+            googleId: id,
+            email: emails[0].value,
+            avatarUrl: avatarUrl,
+          });
+        } else {
+          // Ensure the googleId is updated for existing users
+          user.googleId = id;
         }
+
+        // Generate and assign a token regardless of new or existing user
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
           expiresIn: "1h",
         });
         user.token = token;
+
         await user.save();
         done(null, user);
       } catch (error) {
@@ -45,10 +46,15 @@ passport.use(
   )
 );
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 });
